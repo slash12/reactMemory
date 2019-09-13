0d579db84f1db4f01c49ef9da0c74cdbf308de86
slash12 <ns@esokia-webagency.com>
2019-09-13 17:25:22 +0400

    completed RD operation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'test 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'test 2',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'test 3',
+        completed: true
+      },
+    ]
+  }
+
+  markComplete = (id) => {
+    this.setState ({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });    
+  }
+
+  delTodo = (id) => {
+    this.setState ({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  //console.log(state.todos);
+  render () {    
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo 
+            addTodo={this.addTodo} 
+          />
+          <Todos 
+            todos={this.state.todos} 
+            markComplete={this.markComplete} 
+            delTodo={this.delTodo}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class AddTodo extends Component {
+    state = {
+        title: ''
+    }
+
+    onChange = (e) => this.setState({
+        [e.target.name]: e.target.value
+    });
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({ title: '' });
+    }
+
+    render() {
+        return (
+            <form 
+                style = {{display: 'flex'}}
+                onSubmit = {this.onSubmit}
+            >
+                <input 
+                    type="text" 
+                    name="title" 
+                    placeholder="Add Todo ..." 
+                    style={{flex: '10', padding: '5px'}}
+                    value={this.state.title}
+                    onChange={this.onChange}
+                />
+
+                <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Submit"
+                    style={{flex: '1'}}
+                />
+            </form>
+        )
+    }
+}
